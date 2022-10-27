fde8cfa169a8cd44bd7a4ddab5450caa82f2014d
Artem Egorov <Artem-Egorov13@mail.ru>
2022-10-27 14:49:00 +0300

    change name variable

--- src/main/java/ru/netology/src/Diapason/services/SqrService.java
+++ src/main/java/ru/netology/src/Diapason/services/SqrService.java
@@ -3,13 +3,13 @@ package ru.netology.src.Diapason.services;
 public class SqrService {
 
     public int calcSqr(int start, int finish) {
-        int varible = 0;
+        int numberOfSquares = 0;
 
         for (int i = 10; i <= 99; i++) {
             if (i * i >= start & i * i <= finish) {
-                varible++;
+                numberOfSquares++;
             }
         }
-        return varible;
+        return numberOfSquares;
     }
 }
